06071a7fc9536877a89e016db8b67c6246a460c0
Hyungseock-Yun <bagzein@gmail.com>
2022-08-12 02:45:23 +0900

    vetur

---
+++ vue-cli/src/a.vue
@@ -0,0 +1,18 @@
+<template>
+  <!-- HTML -->
+  <div>header</div>
+</template>
+
+<script>
+export default {
+    methods: {
+        addNum: function() {
+
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
